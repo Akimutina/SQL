c9274700950afe3c9a044024618d2c2f1de15a7d
Aki <akimutina.olga@mail.ru>
2023-05-04 19:46:36 +0700

    first commit

---
+++ src/test/java/ru/netology/sql/test/BankLoginTest.java
@@ -0,0 +1,32 @@
+package ru.netology.sql.test;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.netology.sql.data.DataHelper;
+import ru.netology.sql.data.SQLHelper;
+import ru.netology.sql.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.sql.data.SQLHelper.cleanDatabase;
+
+public class BankLoginTest {
+
+    @AfterAll
+    static void teardown(){
+        cleanDatabase();
+    }
+
+    @Test
+    @DisplayName("Should successfully login to dashboard with exist login add password from sut test data")
+    void shouldSuccessfulLogin(){
+        open("http://localhost:9999");
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+
+}
